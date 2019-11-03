22be868f515eb5476e159119d4e195cde17351ad
HongScarlet <56990874+HongScarlet@users.noreply.github.com>
2019-11-04 01:28:49 +0800

    05. 反轉數字

---
+++ C/20191030_03-inverse_numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <stdlib.h>    /* DEL */ 
+int main()
+{
+  int array[1000];
+  int i=0;
+  char c;
+  printf("Example>>imput 10 20 30\n");
+  printf("Input 10 20 30\n");
+  printf("Output>>imput 30 20 10\n");
+  printf("Please enter numbers\n");
+   do
+   {
+    scanf("%d",&array[i]);
+    i++;
+   }while(c=getchar()!='\n');         /* 判斷是否按了enter */
+
+  printf("Output\n");
+
+  for(int j=i-1;j>=0;j--)  /* 從array的第i-1項印回第0項 */
+  {
+       printf("%d ",array[j]);
+  }
+   system("PAUSE");    /* DEL */
+   return 0;
+}
+
+
