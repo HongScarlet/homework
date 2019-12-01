cb6efa71056e1842fd4c2c785f41c4a7ab3797e9
HongScarlet <56990874+HongScarlet@users.noreply.github.com>
2019-12-02 02:14:46 +0800

    01. 溫度轉換

---
+++ GO/20191127_01-main.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+import "temperature"  // import $GOPATH/src/temperature下的temperature.go
+
+var X string
+var T float32
+
+func main() {
+	fmt.Println("Please enter temp and unit (ex: 100 C): ")
+	fmt.Scanln(&T, &X)
+	// fmt.Scanf  讀取使用者輸入(數值 & 單位)
+	switch X {
+	case "C":
+		temperature.C2F(T)   	// 攝氏轉華氏
+	case "F":
+		temperature.F2C(T)   	// 華氏轉攝氏
+	default:
+		fmt.Println("Error")    // 輸入非C或F則顯示Error
+	}
+}
+
+// import的function必須放置在gopath下的src內
+// 本題中即是把temperature.go放在 $GOPATH/src/temperature之下
